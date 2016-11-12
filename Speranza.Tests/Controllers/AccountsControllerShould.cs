f6a4814267397758c5985ff9d42e87e622ceb942
Zuzka <zuzka.papalova@gmail.com>
2016-11-12 17:01:44 +0100

    Register view can be created from controller

---
+++ Speranza.Tests/Controllers/AccountsControllerShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Speranza.Controllers;
+
+namespace Speranza.Tests.Controllers
+{
+    [TestClass]
+    public class AccountsControllerShould
+    {
+        private AccountsController controller;
+
+        [TestMethod]
+        public void ReturnCorrectView_When_RegisteringNewUser()
+        {
+            InitializeController();
+
+            ViewResult result = controller.Register();
+
+            Assert.AreEqual("Register", result.ViewName);
+
+        }
+
+        private void InitializeController()
+        {
+            controller = new AccountsController();
+        }
+    }
+}
---
+++ Speranza/Controllers/AccountsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Speranza.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        public ViewResult Register()
+        {
+            return View("Register");
+        }
+    }
+}
