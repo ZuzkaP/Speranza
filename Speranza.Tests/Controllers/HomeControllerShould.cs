9d415db59ffaf87ef7f23656cd467a9b96361eee
Zuzka <zuzka.papalova@gmail.com>
2016-11-12 16:31:42 +0100

    Index page is displayed when requested

---
+++ Speranza.Tests/Controllers/HomeControllerShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Speranza.Controllers;
+using System.Web.Mvc;
+
+namespace Speranza.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerShould
+    {
+        private HomeController controller;
+
+        [TestMethod]
+        public void OpenIndexHtml()
+        {
+            InitializeController();
+
+            ViewResult result = controller.Index();
+
+            Assert.AreEqual("Index", result.ViewName);
+
+        }
+
+        private void InitializeController()
+        {
+            controller = new HomeController();
+        }
+    }
+}
