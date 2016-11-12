aa0d30db69ab9dac65e4296a654d5295ab58cb7f
Zuzka <zuzka.papalova@gmail.com>
2016-11-12 16:53:12 +0100

    Login Form view

---
+++ Speranza/Models/UserLoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Speranza.Models
+{
+    public class UserLoginModel
+    {
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
