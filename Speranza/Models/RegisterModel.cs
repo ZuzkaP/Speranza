881223f59e73e4d187bc4a60c0912630e0c62ae1
Zuzka <zuzka.papalova@gmail.com>
2016-11-12 17:16:03 +0100

    Register form draft

---
+++ Speranza/Models/RegisterModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Speranza.Models
+{
+    public class RegisterModel
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
