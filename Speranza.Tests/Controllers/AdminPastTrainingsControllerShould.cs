6660ed3f178ce26c96c322ebc163773423325430
Zuzka <zuzka.papalova@gmail.com>
2017-03-05 18:23:39 +0100

    AdminPastTraining controller basic functionality implemented

---
+++ Speranza.Tests/Controllers/AdminPastTrainingsControllerShould.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Speranza.Services.Interfaces;
+using Moq;
+using Speranza.Controllers;
+using System.Web.SessionState;
+using System.Web.Mvc;
+
+namespace Speranza.Tests.Controllers
+{
+    [TestClass]
+    public class AdminPastTrainingsControllerShould
+    {
+        private Mock<IUserManager> userManager;
+        private Mock<ITrainingsManager> trainingManager;
+        private Mock<IDateTimeService> dateTimeService;
+        private AdminPastTrainingsController controller;
+
+        private const string USER_EMAIL = "test";
+
+        [TestMethod]
+        public void ReturnToCalendar_When_ClickOnAdminUsers_And_UserIsNotLogin()
+        {
+            InitializeAdminTrainingsController();
+            userManager.Setup(r => r.IsUserLoggedIn(controller.Session)).Returns(false);
+            userManager.Setup(r => r.IsUserAdmin(controller.Session)).Returns(false);
+
+            ActionResult result = controller.AdminTrainings();
+
+            Assert.IsInstanceOfType(result, typeof(RedirectToRouteResult));
+            Assert.AreEqual("Home", ((RedirectToRouteResult)result).RouteValues["controller"]);
+            Assert.AreEqual("Index", ((RedirectToRouteResult)result).RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void ReturnToCalendar_When_ClickOnAdminUsers_And_UserIsNotAdmin()
+        {
+            InitializeAdminTrainingsController();
+            userManager.Setup(r => r.IsUserAdmin(controller.Session)).Returns(false);
+
+            ActionResult result = controller.AdminTrainings();
+
+            Assert.IsInstanceOfType(result, typeof(RedirectToRouteResult));
+            Assert.AreEqual("Calendar", ((RedirectToRouteResult)result).RouteValues["controller"]);
+            Assert.AreEqual("Calendar", ((RedirectToRouteResult)result).RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void ShowView_When_ClickOnAdminUsers_And_UserIsAdmin()
+        {
+            InitializeAdminTrainingsController();
+
+            ActionResult result = controller.AdminTrainings();
+
+            Assert.AreEqual("AdminTrainings", ((ViewResult)result).ViewName);
+        }
+
+        private void InitializeAdminTrainingsController()
+        {
+            userManager = new Mock<IUserManager>();
+            trainingManager = new Mock<ITrainingsManager>();
+            dateTimeService = new Mock<IDateTimeService>();
+            controller = new AdminPastTrainingsController(userManager.Object, trainingManager.Object, dateTimeService.Object);
+            SessionStateItemCollection sessionItems = new SessionStateItemCollection();
+            controller.ControllerContext = new FakeControllerContext(controller, sessionItems);
+            controller.Session["Email"] = USER_EMAIL;
+            userManager.Setup(r => r.IsUserLoggedIn(controller.Session)).Returns(true);
+            userManager.Setup(r => r.IsUserAdmin(controller.Session)).Returns(true);
+        }
+    }
+}
---
+++ Speranza/Controllers/AdminPastTrainingsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Speranza.Services.Interfaces;
+using Speranza.App_Start;
+
+namespace Speranza.Controllers
+{
+    public class AdminPastTrainingsController : Controller
+    {
+        private IUserManager userManager;
+        private ITrainingsManager trainingManager;
+        private IDateTimeService dateTimeService;
+
+        public AdminPastTrainingsController(IUserManager userManager, ITrainingsManager trainingManager, IDateTimeService dateTimeService)
+        {
+            this.userManager = userManager;
+            this.trainingManager = trainingManager;
+            this.dateTimeService = dateTimeService;
+        }
+
+        public AdminPastTrainingsController() : this(Initializer.UserManager, Initializer.TrainingsManager, Initializer.DateTimeService)
+        {
+
+        }
+
+        // GET: AdminPastTrainings
+        public ActionResult AdminTrainings()
+        {
+            if (userManager.IsUserLoggedIn(Session))
+            {
+                if (!userManager.IsUserAdmin(Session))
+                {
+                    return RedirectToAction("Calendar", "Calendar");
+                }
+                return View("AdminTrainings");
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
