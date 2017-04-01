f7bc6ce89c53d7ed07ff58b14017d9db4b2af5d2
Zuzka <zuzka.papalova@gmail.com>
2017-04-01 20:18:36 +0200

    Database implementation started

---
+++ Speranza.Database/Database.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Speranza.Common.Data;
+using Speranza.Database.Data.Interfaces;
+using System.Data.SqlClient;
+
+namespace Speranza.Database
+{
+    public class Database : IDatabaseGateway
+    {
+        private const string SERVER_NAME = "localhost";
+        private const string DATABASE_NAME = "SPERANZADB";
+        private SqlConnection connection;
+
+        public Database()
+        {
+            string connetionString = null;
+            connetionString = "Data Source=" + SERVER_NAME + ";Initial Catalog=" + DATABASE_NAME;
+            connection = new SqlConnection(connetionString);
+        }
+        public void AddUserToTraining(string email, string trainingID, DateTime timeOfSignUp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AllowSigningUpToTrainings(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CancelTraining(string trainingID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangePassword(string email, string newpasswordhash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConfirmParticipation(string trainingID, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateNewTraining(string trainingID, DateTime dateTime, string trainer, string trainingDescription, int capacity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateRecurringTrainingTemplate(RecurringTrainingTemplate recurringTrainingTemplate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisproveParticipation(string trainingID, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ForbidSigningUpToTrainings(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<IUser> GetAdmins()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetAllowedToSignUpFlag(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ITraining> GetAllTrainings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<IUser> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<IUserInTraining> GetAllUsersInTrainingWithZeroEntranceFlag()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ITraining> GetDayTrainings(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<string> GetEmailsOfAllUsersInTraining(string trainingID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetLastTemplateGenerationDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<IUserInTraining> GetNonProcessedUsersInTrainingBeforeDate(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetNumberOfVisits(string email, DateTime currentDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetSignOffLimit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<IRecurringTrainingTemplate> GetTemplates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<IRecurringTrainingTemplate> GetTemplatesForTheDay(int day)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITraining GetTrainingData(string trainingID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTrainingsCountAfterDate(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTrainingsCountBeforeDate(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ITraining> GetTrainingsForUser(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IUser GetUserData(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<IUser> GetUsersInTraining(string trainingID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsUserAlreadySignedUpInTraining(string email, string trainingID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IUser LoadUser(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterNewUser(string email, string name, string password, string phoneNumber, string surname)
+        {
+            //INSERT INTO table_name(column1, column2, column3, ...) VALUES(value1, value2, value3, ...);
+
+            string sql = string.Format("INSERT INTO Users(email,name,surname,phoneNumber,password) VALUES( '{0}','{1}','{2}','{3}','{4}');",
+                email,name,surname,phoneNumber,password);
+               
+            ExecuteSql(sql);
+        }
+
+       
+        public void RemoveTrainingTemplate(int day, int time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveUserFromTraining(string email, string trainingID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAdminRole(string email, bool isAdmin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAlreadyProcessedFlag(IUserInTraining userInTraining)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLastTemplateGenerationDate(DateTime dateTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetSignOffLimit(int hoursLimit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetTrainer(string trainingID, string trainer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetTrainingCapacity(string trainingID, int capacity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetTrainingDescription(string trainingID, string trainingDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetUserCategory(string email, UserCategories category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetZeroEntranceFlag(IUserInTraining userInTraining, bool flag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SignOutUserFromAllTrainingsAfterDate(string email, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateCountOfFreeSignUps(string email, int changeNumberOfSignUps)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUserData(string email, string name, string surname, string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        //SELECT column1, column2, ...FROM table_name;
+        public bool UserExists(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteSql(string sql)
+        {
+            try
+            {
+                connection.Open();
+                var command = new SqlCommand(sql, connection);
+                command.ExecuteNonQuery();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void ExecuteSqlWithResult(string sql)
+        {
+            try
+            {
+                connection.Open();
+                var command = new SqlCommand(sql, connection);
+                var reader = command.ExecuteReader();
+                reader.
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+
+    }
+}
--- Speranza/App_Start/Initializer.cs
+++ Speranza/App_Start/Initializer.cs
@@ -28,7 +28,8 @@ namespace Speranza.App_Start
 
         static  Initializer()
         {
-            Db = InMemoryDatabase.Instance;
+            //Db = InMemoryDatabase.Instance;
+            Db = new Database.Database();
             Hasher = new Hasher();
             DateTimeService = new DateTimeService();
             Factory = new ModelFactory();
